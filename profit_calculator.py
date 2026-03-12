1ac19e4efd580c35d8bd45654dec22ea4bb152c1
mepa33z <162494270+mepa33z@users.noreply.github.com>
2026-03-12 08:20:21 -0400

    Implement profit calculation in profit_calculator.py

---
+++ profit_calculator.py
@@ -0,0 +1,27 @@
+from config.system_settings.dispatch_settings import *
+
+def calculate_profit(load):
+
+    #Rate per mile
+    rate_per_mile = rate / miles
+
+    #Fuel estimate
+    gallons_used = miles/ TRUCK_MPG
+    fuel_cost = gallons_used * FUEL_PRICE_PER_GALLON
+
+    #Driver pay
+    driver_pay = miles * DRIVER_PAY_PER_MILE
+
+    #Dispatcher fee
+    dispatch_fee = rate * DISPATCH_PERCENTAGE
+
+    #Carrier profit
+    carrier_profit = rate - fuel_cost - driver_pay - dispatch_fee
+
+    return {
+        "rate_per_mile": rate_per_mile,
+        "fuel_cost": fuel_cost,
+        "driver_pay": driver_pay,
+        "dispatch_commission": dispatch_fee,
+        "carrier_profit": carrier_profit
+    }
